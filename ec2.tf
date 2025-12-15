40b4405d39ab8e16c972de367a652611cfb4530a
swapnil641 <swapnilpawar641@gmail.com>
2025-12-15 19:27:15 +0530

    Initial commit

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 #key pair for login
 resource "aws_key_pair" "my_key" {
-  key_name = "terra-key-ec2"
+  key_name   = "terra-key-ec2"
   public_key = file("terra-key-ec2.pub")
 }
 #VPC & Security Group
@@ -8,57 +8,57 @@ resource "aws_default_vpc" "default" {
 
 }
 resource "aws_security_group" "my_security_group" {
-  name = "automate-sg"
+  name        = "automate-sg"
   description = "this will add a TF generated security group"
-  vpc_id = aws_default_vpc.default.id #interpolation
+  vpc_id      = aws_default_vpc.default.id #interpolation
 
-#inbound rules
-ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+  #inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "SSH Open"
-}
-ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "HTTP Open"
-}
-ingress {
-    from_port = 8000
-    to_port = 8000
-    protocol = "tcp"
+  }
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Flask App Open"
-}
-#outbound rules
-egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+  }
+  #outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     description = "All Access Open"
-}
+  }
   tags = {
     name = "automate-sg"
   }
 }
 #ec2 instance
 resource "aws_instance" "my_instance" {
-  key_name = aws_key_pair.my_key.key_name
-  security_groups = [ aws_security_group.my_security_group.name ]
-  instance_type = var.ec2_instance_type
-  ami = var.ec2_ami_id #Ubuntu Machine
-  user_data = file("install_nginx.sh")
+  key_name        = aws_key_pair.my_key.key_name
+  security_groups = [aws_security_group.my_security_group.name]
+  instance_type   = var.ec2_instance_type
+  ami             = var.ec2_ami_id #Ubuntu Machine
+  user_data       = file("install_nginx.sh")
 
   root_block_device {
     volume_size = var.ec2_root_storage_size
     volume_type = "gp3"
   }
-    tags = {
-        Name = "TWS-Automate"
-    }
+  tags = {
+    Name = "TWS-Automate"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 #setting a some variable as a default
 variable "ec2_instance_type" {
   default = "t2.micro"
-  type = string
+  type    = string
 }
 
 variable "ec2_root_storage_size" {
   default = 15
-  type = number
+  type    = number
 }
 
 variable "ec2_ami_id" {
   default = "ami-02b8269d5e85954ef"
-  type = string
+  type    = string
 }
